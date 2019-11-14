ca67366b74168b3daa63be8f782881888392f5f8
chwei <weicc1989@gmail.com>
2019-11-14 16:15:17 +0800

    update: 辗转相除求最大公约数：Max_common_divisor.py

---
+++ Max_common_divisor.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+'''
+@Author: Chuncheng Wei
+@Email: weicc1989@gmail.com
+@Date: 2019-11-05 17:34:18
+@LastEditTime: 2019-11-05 17:45:44
+@Description: 
+@FilePath: /python/Max_common_divisor.py
+'''
+
+def max_common_divisor(a, b):
+
+    def mcd(x, y):
+        """
+        get max common divisor
+        Args: x > y
+        Returns: y, x % y
+        """
+        r = x % y
+        if r == 0:
+            return y
+        else:
+            return mcd(y, r)
+
+    if a < b:
+        a, b = b, a
+
+    return mcd(a, b)
+
+max_common_divisor(12, 40)
