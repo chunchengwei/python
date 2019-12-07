f147a6456327864012ff0088141f11d89fbcbcfa
chwei <weicc1989@gmail.com>
2019-12-07 14:17:13 +0800

    temp update

--- chi2_to_sigma.py
+++ chi2_to_sigma.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python
+# encoding: utf-8
 '''
 @Author: Chuncheng Wei
 @Email: weicc1989@gmail.com
 @Date: 2019-11-14 16:16:51
 @LastEditTime: 2019-11-14 16:38:31
-@Description: 
+@Description:
 @FilePath: /python/chi2_to_sigma.py
 '''
 
+import sys
 from scipy import stats
+
 
 def chi2_to_sigma(chi2, df):
     """chi-square value to sigma
-    
+
     Args:
         chi2 ([type]): chi2 value
         df ([type]): d.o.f
-    
+
     Returns:
         [type]: sigma
     """
     sigma = stats.chi2.isf(stats.chi2.sf(chi2, df), 1)**0.5
     return sigma
 
-    
-chi2_to_sigma(200, 2)
+
+chi2, df = sys.argv
+sigma = chi2_to_sigma(chi2, df)
+print('(chi2: {}, df: {}): --- > sigma: {}'.format(chi2, df, sigma))
